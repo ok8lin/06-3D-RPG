f0e4fd8dd92f7473274a29a63d480953a245d6fb
ok8lin <harriskn@iu.edu>
2021-12-11 20:14:33 -0500

    Completed Project

---
+++ Player/NPC/NPC.gd
@@ -0,0 +1,27 @@
+extends KinematicBody
+
+onready var Dialogue = get_node ("/root/Game/UI/Dialogue")
+
+var dialogue = [
+	"Welcome to the game! (Press E to continue)"
+	,"Your life depends on your speed and accuracy."
+	,"Shoot the five targets and then defeat the drone before time runs out!"
+	,"The challenge starts as soon as you press E."
+]
+
+func _ready():
+	$AnimationPlayer.play("Idle")
+	Dialogue.connect("finished_dialogue", self, "finished")
+
+
+func _on_Area_body_entered(body):
+	Dialogue.start_dialogue(dialogue)
+
+func _on_Area_body_exited(body):
+	Dialogue.hide_dialogue()
+
+func finished():
+	get_node("/root/Game/Target_container").show()
+	Global.timer = 120
+	Global.update_time()
+	get_node("/root/Game/UI/Timer").start()
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# 06-3D-RPG
-last project for mschc220
+# 3D-Character
+
+An animated 3D character that can be used in the 3D-RPG project.
+
+Based on the [Kenney Character Assets](https://kenney.itch.io/kenney-character-assets) provided by kenney.nl. The model and animations were compiled in Blender and then imported into Godot.
+
+## Implementation
+Created in Blender 2.9 and Godot 3.3
+
+## Created By
+BL-MSCH-C220-S21
